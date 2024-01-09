4117182547d1de23485e3ff89ac9a503fe0a7ab5
habanoz <huseyinabanox@gmail.com>
2024-01-09 13:09:11 +0300

    Revert "apply_chat_template give template argument."
    This reverts commit 23a919effd62fa17995458a473dc466dce424c08.

--- utils.py
+++ utils.py
@@ -1,7 +1,3 @@
 def load_template(tokenizer):
     with open("templates/default.txt", "r") as file:
         tokenizer.chat_template = file.read()
-
-def get_template():
-    with open("templates/default.txt", "r") as file:
-        return file.read()
